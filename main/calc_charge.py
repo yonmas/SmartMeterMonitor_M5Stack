1e08842e19430fec04fad1aafb3d772df9c815cb
yonmas <>
2023-08-12 14:31:37 +0900

    電気料金の計算法を見直し（2023.6.1 東京電力）

--- main/calc_charge.py
+++ main/calc_charge.py
@@ -1,24 +1,29 @@
 def tepco(contract, power):
     """
     東京電力での電気料金計算（従量電灯B）
+    2023.6.1 料金改定版
 
     基本料金
-      契約アンペア数 : 基本料金
-              　 10A :   286.00円
-              　 15A :   429.00円
-            　   20A :   572.00円
-          　     30A :   858.00円
-          　     40A : 1,144.00円
-          　     50A : 1,430.00円
-          　     60A : 1,716.00円
+      10A :   295.24円   base10
+      15A :   442.86円   base15
+      20A :   590.48円   base20
+      30A :   885.72円   base30
+      40A : 1,180.96円   base40
+      50A : 1,476.20円   base50
+      60A : 1,771.44円   base60
 
-    従量料金
-      〜120lWh : 19.88円/kWh
-      〜300kWh : 26.48円/kwh
-      〜       : 30.57円/kwh
+    従量料金（2023.4.1の燃料費調整単価 -8.78円 込み）
+      〜120lWh : 30.00円/kWh    rate1
+      〜300kWh : 36.60円/kwh    rate2
+      〜       : 40.69円/kwh    rate3
 
-    電気料金 = 基本料金 + 従量料金 + （燃料費調整額 + 再エネ発電賦課金 - 口座振替割引(55円)）
-      ※ (基本料金 + 従量料金) 部分のみを計算
+    燃料費調整単価（毎月更新）
+      -11.21円/kWh  nenchou
+
+    再エネ発電賦課金（〜2024.4）
+      1.40円/kWh    saiene
+
+    電気料金 = 基本料金 + 従量料金 + 燃料費調整額 + 再エネ発電賦課金（税込）
 
     Parameters
     ----------
@@ -33,117 +38,36 @@ def tepco(contract, power):
         電気料金
 
     """
-    fee = {'10': 286.00, '15': 429.00, '20': 572.00, '30': 858.00, '40': 1144.00, '50': 1430.00, '60': 1716.00}[contract]
+
+    base10 =  295.24
+    base15 =  442.86
+    base20 =  590.48
+    base30 =  885.72
+    base40 = 1180.96
+    base50 = 1476.20
+    base60 = 1771.44
+
+    rate1  = 30.00
+    rate2  = 36.60
+    rate3  = 40.69
+
+    nencho = -11.21 # 2023.8
+    saisei = 1.40   # 〜 2024.4
+
+    fee = {'10': base10, '15': base15, '20': base20, '30': base30, '40': base40, '50': base50, '60': base60}[contract]
+
+    power = int(power)
 
     if power <= 120:
-        fee += 19.88 * power
+        fee += rate1 * power
     elif power <= 300:
-        fee += 19.88 * 120
-        fee += 26.48 * (power - 120)
+        fee += rate1 * 120
+        fee += rate2 * (power - 120)
     else:
-        fee += 19.88 * 120
-        fee += 26.48 * 180
-        fee += 30.57 * (power - 120 - 180)
-    return int(fee + (power * 3.36))
-
-
-# 以下オリジナル
-
-# TOKYO GASの料金計算
-# https://home.tokyo-gas.co.jp/power/ryokin/tanka/index.html
-
-
-def tokyo_gas_1s(contract, power):
-    """
-    TOKYO GAS「ずっとも電気1S」での電気料金計算
-
-    Parameters
-    ----------
-    contract : str
-        契約アンペア数
-    power : float
-        前回検針後の使用電力量（kWh）
+        fee += rate1 * 120
+        fee += rate2 * 180
+        fee += rate3 * (power - 120 - 180)
     
-    Returns
-    -------
-    fee: int
-        電気料金
-    """
-    fee = {
-        '10': 286.00,
-        '15': 429.00,
-        '20': 572.00,
-        '40': 1144.00,
-        '50': 1430.00,
-        '60': 1716.0
-    }[contract]
+    fee += nencho * power + int(saisei * power) # 燃料費調整額・再エネ発電賦課金 加算
 
-    if power <= 120:
-        fee += 19.85 * power
-    elif power <= 300:
-        fee += 19.85 * 120
-        fee += 25.35 * (power - 120)
-    else:
-        fee += 19.85 * 120
-        fee += 25.35 * 300
-        fee += 27.48 * (power - 120 - 300)
     return int(fee)
-
-
-def tokyo_gas_1(contract, power):
-    """
-    TOKYO GAS「ずっとも電気1」での電気料金計算
-
-    Parameters
-    ----------
-    contract : str
-        契約アンペア数
-    power : float
-        前回検針後の使用電力量（kWh）
-    
-    Returns
-    -------
-    fee: int
-        電気料金
-    """
-    fee = {'30': 858.00, '40': 1144.00, '50': 1430.00, '60': 1716.00}[contract]
-
-    if power <= 140:
-        fee += 23.67 * power
-    elif power <= 350:
-        fee += 23.67 * 140
-        fee += 23.88 * (power - 140)
-    else:
-        fee += 23.67 * 140
-        fee += 23.88 * 350
-        fee += 26.41 * (power - 140 - 350)
-    return int(fee)
-
-
-def tokyo_gas_2(contract, power):
-    """
-    TOKYO GAS「ずっとも電気2」での電気料金計算
-
-    Parameters
-    ----------
-    contract : str
-        契約アンペア数
-    power : float
-        前回検針後の使用電力量（kWh）
-    
-    Returns
-    -------
-    fee: int
-        電気料金
-    """
-    fee = 286.00
-    if power <= 360:
-        fee += 19.85 * power
-    else:
-        fee += 23.63 * 360
-        fee += 26.47 * (power - 360)
-    return int(fee)
-
-
-if __name__ == '__main__':
-    print(tokyo_gas_1('50', 339))
